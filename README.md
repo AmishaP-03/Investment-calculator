43487e5b22d8beea010c304a3a73ede2fd21b8d2
Amisha PRIYA <priyaamisha03@gmail.com>
2024-02-03 17:03:01 +0530

    Display results

--- README.md
+++ README.md
@@ -6,3 +6,12 @@ Required investment parameters to compute ROI:
 2. Annual investment -> Amount that the user intends to invest annually
 3. Expected return -> Expected annual rate of return
 4. Duration -> Time period (in years) for which the user intends to hold their investment
+
+The result is computed based on the above input parameters and is displayed in a tabular format whose content are:
+
+1. Year -> Year of investment
+2. Investment value -> Current net worth
+3. Interest (for the current year) -> Rate of interest earned in the current year
+4. Total interest -> Total interest earned till now
+5. Investment capital -> Total amount invested till now
+
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,5 +1,33 @@
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 export default function Results({userInput}) {
     const resultsData = calculateInvestmentResults(userInput);
+
+    return (
+        <table id="result">
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Investment value</th>
+                    <th>Interest (for the current year)</th>
+                    <th>Total interest</th>
+                    <th>Investment capital</th>
+                </tr>
+            </thead>
+            <tbody>
+                {resultsData.map((resultPerYear) => {
+                    const totalInterest = resultPerYear.valueEndOfYear - resultPerYear.annualInvestment * resultPerYear.year - userInput.initialInvestment;
+                    return (
+                        <tr key={resultPerYear.year}>
+                            <td>{resultPerYear.year}</td>
+                            <td>{formatter.format(resultPerYear.valueEndOfYear)}</td>
+                            <td>{formatter.format(resultPerYear.interest)}</td>
+                            <td>{formatter.format(totalInterest)}</td>
+                            <td>{formatter.format(resultPerYear.valueEndOfYear - totalInterest)}</td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
 }
